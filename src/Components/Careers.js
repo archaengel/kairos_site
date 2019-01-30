c987a97f9c03111a09eef2d18ccf0b8423b144ab
Archaengel <god11341258@gmail.com>
2019-01-30 15:03:55 -0800

    Update Career copy

--- src/Components/Careers.js
+++ src/Components/Careers.js
@@ -15,13 +15,11 @@ const Careers = () => (
     <div className="container flex flex-col justify-center w-3/5 my-8 sm:my-8 md:my-16">
       <article className="">
         <h2 className="mb-6 text-3xl sm:text-4xl sm:mb-6 md:mb-10 md:text-5xl">
-         ∞ 
+         join the <div className="inline-block text-red">fun</div> 
         </h2>
         <p className="font-mono font-bold text-lg text-justify sm:text-2xl md:text-2xl">
-          We are always looking for passionate and motivated individuals to work with. 
-          If you share our interest in the intersection of design, technology, mathematics,
-          and effective and affective pedagogy don't 
-          hesitate send us your info at <a href="mailto:jobs@kairoscounselors.com">jobs@kairoscounselors.com</a>
+          We are always looking for motivated individuals to work with. If you have something
+          to contribute, don't hesitate to send your information to <a href="mailto:jobs@kairoscounselors.com">jobs@kairoscounselors.com</a>
         </p>
       </article>
       <img src={commDiag} className="container rot-135 w-40 h-40 my-16 sm:w-50 sm:h-50 md:w-64 sm:my-16 md:h-64 md:my-8" alt="Category of objects X, Y, and Z, and morphisms f, g, f;g"/>
