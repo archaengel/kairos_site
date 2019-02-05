77952fe1fde7ac1c78f72c143a650bc73460e687
Archaengel <god11341258@gmail.com>
2019-02-05 13:53:06 -0800

    Improve nav link spacing

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -5,11 +5,11 @@ import {Link} from 'react-router-dom';
 const Navigation = () => {
   return(
     <nav className="w-screen flex justify-center">
-      <Link to="/" className="m-1 text-base sm:m-1 md:m-1 sm:text-xl md:text-xl lg:text-3xl lg:m-3 text-center text-blue font-mono">Home</Link>
-      <Link to="/about" className="m-1 text-base sm:m-1 md:m-1 sm:text-xl md:text-xl lg:text-3xl lg:m-3 text-center text-blue font-mono">About</Link>
-      <Link to="/promotional" className="m-1 sm:m-1 text-base md:m-1 sm:text-xl md:text-xl lg:text-3xl lg:m-3 text-center text-blue font-mono">Promotional</Link>
-      <Link to="/contact" className="m-1 text-base sm:m-1 md:m-1 sm:text-xl md:text-xl lg:text-3xl lg:m-3 text-center text-blue font-mono">Contact</Link>
-      <Link to="/jobs" className="m-1 text-base sm:m-1 md:m-1 sm:text-xl md:text-xl lg:text-3xl lg:m-3 text-center text-blue font-mono">Jobs</Link>
+      <Link to="/" className="m-2 text-lg sm:m-2 md:m-2 sm:text-xl md:text-xl lg:text-3xl lg:m-3 text-center text-blue font-mono">Home</Link>
+      <Link to="/about" className="m-2 text-lg sm:m-2 md:m-2 sm:text-xl md:text-xl lg:text-3xl lg:m-3 text-center text-blue font-mono">About</Link>
+      <Link to="/promotional" className="m-2 sm:m-2 text-lg md:m-2 sm:text-xl md:text-xl lg:text-3xl lg:m-3 text-center text-blue font-mono">Promotional</Link>
+      <Link to="/contact" className="m-2 text-lg sm:m-2 md:m-2 sm:text-xl md:text-xl lg:text-3xl lg:m-3 text-center text-blue font-mono">Contact</Link>
+      <Link to="/jobs" className="m-2 text-lg sm:m-2 md:m-2 sm:text-xl md:text-xl lg:text-3xl lg:m-3 text-center text-blue font-mono">Jobs</Link>
     </nav>
   )
 };
